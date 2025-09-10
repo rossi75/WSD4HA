bb34912dc19129c4c9c703a863b4311bcfedeca2
rossi75 <91349581+rossi75@users.noreply.github.com>
2025-09-10 11:29:02 +0200

    Update main.py

--- wsd/main.py
+++ wsd/main.py
@@ -1,1 +1,13 @@
+import asyncio
+from webserver import start_http_server
+from wsd import discovery_listener, heartbeat_monitor
 
+async def main():
+    await asyncio.gather(
+        start_http_server(),
+        discovery_listener(),
+        heartbeat_monitor(),
+    )
+
+if __name__ == "__main__":
+    asyncio.run(main())
