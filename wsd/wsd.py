4bc268f3f984d6c5c6e1076001d4dc6847b8fab9
rossi75 <91349581+rossi75@users.noreply.github.com>
2025-09-10 13:00:38 +0200

    Update wsd.py

--- wsd/wsd.py
+++ wsd/wsd.py
@@ -176,3 +176,24 @@ async def check_scanner(scanner):
         logger.info(f"[Heartbeat OK] {scanner.ip} lebt noch")
     except Exception as e:
         logger.warning(f"[Heartbeat FAIL] {scanner.ip}: {e}")
+
+# ---------------- HTTP/SOAP Server ----------------
+async def handle_scan_job(request):
+    logger.info("{datetime.datetime.now():%Y%m%d %H%M%S} [SCAN] Scan-Job started")
+    data = await request.read()
+    logger.info(f"{datetime.datetime.now():%Y%m%d %H%M%S} [SCAN] Received first Bytes: {len(data)}")
+    #logger.debug(f"[SCAN] Received first Bytes: {len(data)}")
+    filename = WSD_SCAN_FOLDER / f"scan-{datetime.datetime.now():%Y%m%d-%H%M%S}.bin"
+    try:
+        with open(filename, "wb") as f:
+            f.write(data)
+        logger.info(f"{datetime.datetime.now():%Y%m%d %H%M%S} [SCAN] Scan finished: {filename} ({len(data)/1024:.1f} KB)")
+    except Exception as e:
+        logger.error(f"{datetime.datetime.now():%Y%m%d %H%M%S} [SCAN] Error while saving: {e}")
+    return web.Response(text="""
+        <soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope">
+            <soap:Body>
+                <ScanJobResponse>OK</ScanJobResponse>
+            </soap:Body>
+        </soap:Envelope>
+    """, content_type='application/soap+xml')
