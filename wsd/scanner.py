5cdf82b0f2f0dfe626dbc478a95dfbc74861f151
rossi75 <91349581+rossi75@users.noreply.github.com>
2025-09-11 15:16:37 +0200

    Update scanner.py

--- wsd/scanner.py
+++ wsd/scanner.py
@@ -146,7 +146,7 @@ class Scanner:
 
         logger.info(f"{datetime.datetime.now():%Y-%m-%d %H:%M:%S} [Scanner:{self.ip}] Metadaten: {self.name} | FW={self.firmware} | SN={self.serial}")
 
-    # wird aufgerufen wenn ein Scanner offline gesetzt wird
+    # -----------------  wird aufgerufen wenn ein Scanner offline gesetzt wird  -----------------
     def mark_offline(self):
         if self.online:
             logger.warning(f"[Scanner Offline] {self.ip} ({self.friendly_name or self.name})")
@@ -156,4 +156,11 @@ class Scanner:
             self.remove_after = self.offline_since + datetime.timedelta(seconds=self.max_age)
 
 #SCANNERS = {}  # key = UUID oder IP
+        # -----------------  Nach jedem Update: Liste loggen  -----------------
+    def list_scanners():
+        logger.info("[SCANNERS] registered Scanners:")
+#        for s in SCANNERS.values():
+#            logger.info(f"  - {s.name} ({s.ip}, {s.uuid})")
+        for i, s in enumerate(SCANNERS.values(), start=1):
+            logger.info(f"[{i}] {s.name} ({s.ip}) UUID={s.uuid} Online={s.online}")
 
