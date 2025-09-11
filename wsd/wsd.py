d4641df8705fdf910b975041acc4388639eef267
rossi75 <91349581+rossi75@users.noreply.github.com>
2025-09-11 15:47:01 +0200

    Update wsd.py

--- wsd/wsd.py
+++ wsd/wsd.py
@@ -152,6 +152,7 @@ async def discovery_listener():
         elif "Bye" in action_text:
             #uuid = root.find(".//{http://schemas.xmlsoap.org/ws/2004/08/addressing}Address")
             #uuid = uuid.text.strip() if uuid is not None else f"UUID-{ip}"
+            logger.info(f"{datetime.datetime.now():%Y-%m-%d %H:%M:%S} [WSD:BYE] Bye for uuid: {SCANNERS[uuid].name} {uuid}")
             if uuid in SCANNERS:
                 logger.info(f"{datetime.datetime.now():%Y-%m-%d %H:%M:%S} [WSD:BYE] Scanner offline: {SCANNERS[uuid].name} ({ip})")
                 del SCANNERS[uuid]
