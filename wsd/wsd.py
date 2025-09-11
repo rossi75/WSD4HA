a560c7dff47c29c66a1127792953d929300aa625
rossi75 <91349581+rossi75@users.noreply.github.com>
2025-09-11 22:47:44 +0200

    Update wsd.py

--- wsd/wsd.py
+++ wsd/wsd.py
@@ -277,12 +277,15 @@ async def heartbeat_monitor():
                 to_remove.append(scanner)
 
         # welche Scanner sollen entfernt werden?
-        for s in to_remove:
+       logger.info(f"{datetime.datetime.now():%Y-%m-%d %H:%M:%S} [Heartbeat] checking for Scanners to remove from known list")
+       for s in to_remove:
             logger.info(f"[Heartbeat]     --> Removing {scanner.ip} ({scanner.friendly_name or scanner.name}) from list")
             scanners.remove(s)
 
 #        await asyncio.sleep(30)
+        logger.info(f"{datetime.datetime.now():%Y-%m-%d %H:%M:%S} [Heartbeat] goodbye")
         await asyncio.sleep(10)
+        logger.info(f"{datetime.datetime.now():%Y-%m-%d %H:%M:%S} [Heartbeat] back in town")
 
 
 
