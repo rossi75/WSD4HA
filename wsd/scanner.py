abc82acaea8b234d09ef1a40358134ec5b20234f
rossi75 <91349581+rossi75@users.noreply.github.com>
2025-09-11 16:02:38 +0200

    Update scanner.py

--- wsd/scanner.py
+++ wsd/scanner.py
@@ -158,10 +158,10 @@ class Scanner:
 
 #SCANNERS = {}  # key = UUID oder IP
         # -----------------  Nach jedem Update: Liste loggen  -----------------
-def list_scanners():
-    logger.info("[SCANNERS] registered Scanners:")
-#        for s in SCANNERS.values():
-#            logger.info(f"  - {s.name} ({s.ip}, {s.uuid})")
-    for i, s in enumerate(SCANNERS.values(), start=1):
-        logger.info(f"[{i}] {s.name} ({s.ip}) UUID={s.uuid} Online={s.online}")
+#def list_scanners():
+#    logger.info("[SCANNERS] registered Scanners:")
+##        for s in SCANNERS.values():
+##            logger.info(f"  - {s.name} ({s.ip}, {s.uuid})")
+#    for i, s in enumerate(SCANNERS.values(), start=1):
+#        logger.info(f"[{i}] {s.name} ({s.ip}) UUID={s.uuid} Online={s.online}")
 
