c11c674883d5606f18a1fbb7e2ad3d394cbf9ea5
rossi75 <91349581+rossi75@users.noreply.github.com>
2025-09-12 10:53:12 +0200

    Update webserver.py

--- wsd/webserver.py
+++ wsd/webserver.py
@@ -10,7 +10,8 @@ import re
 import xml.etree.ElementTree as ET
 import subprocess
 #from wsd import UDP_listener_3702, heartbeat_monitor, handle_scan_job
-from config import WSD_HTTP_PORT, WSD_OFFLINE_TIMEOUT, WSD_SCAN_FOLDER
+#from config import WSD_HTTP_PORT, WSD_OFFLINE_TIMEOUT, WSD_SCAN_FOLDER
+from config import OFFLINE_TIMEOUT, SCAN_FOLDER, HTTP_PORT
 
 logging.basicConfig(level=logging.INFO, format='[%(levelname)s] %(message)s')
 logger = logging.getLogger("wsd-addon")
@@ -18,7 +19,7 @@ logger = logging.getLogger("wsd-addon")
 # ---------------- WebUI ----------------
 async def status_page(request):
     # Dateien
-    files = sorted(WSD_SCAN_FOLDER.iterdir(), reverse=True)[:WSD_MAX_FILES]
+    files = sorted(WSD_SCAN_FOLDER.iterdir(), reverse=True)[:MAX_FILES]
     file_list = ''
     for f in files:
         stat = f.stat()
@@ -33,7 +34,7 @@ async def status_page(request):
         delta = (now - s.last_seen).total_seconds()
         if delta > WSD_OFFLINE_TIMEOUT:
             s.online = False
-        color = "green" if s.online else ("orange" if delta < 2*WSD_OFFLINE_TIMEOUT else "red")
+        color = "green" if s.online else ("orange" if delta < 2*OFFLINE_TIMEOUT else "red")
         formats = ", ".join(s.formats)
         #formats = ", ".join(s.types)
         scanner_list += f"<tr style='color:{color}'><td>{s.name}</td><td>{s.ip}</td><td>{s.mac or ''}</td><td>{s.uuid or ''}</td><td>{formats}</td><td>{'Online' if s.online else 'Offline'}</td><td>{s.last_seen.strftime('%Y-%m-%d %H:%M:%S')}</td></tr>"
@@ -50,7 +51,7 @@ async def status_page(request):
         </head>
         <body>
             <h1>WSD Add-on running</h1>
-            <h2>Last {WSD_MAX_FILES} Scans:</h2>
+            <h2>Last {MAX_FILES} Scans:</h2>
             <table>
                 <tr><th>Filename</th><th>Date/Time</th><th>Size (KB)</th></tr>
                 {file_list}
@@ -73,6 +74,6 @@ async def start_http_server():
     await runner.setup()
     
     # An alle Interfaces binden (0.0.0.0) -> wichtig für Docker / HA
-    site = web.TCPSite(runner, "0.0.0.0", WSD_HTTP_PORT)
+    site = web.TCPSite(runner, "0.0.0.0", HTTP_PORT)
     await site.start()
-    logger.info(f"[*] HTTP SOAP Server running on Port {WSD_HTTP_PORT}")
+    logger.info(f"[*] HTTP SOAP Server running on Port {HTTP_PORT}")
