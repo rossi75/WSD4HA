d7110362bc85a042236f9321e7462697aad0903f
rossi75 <91349581+rossi75@users.noreply.github.com>
2025-09-12 11:48:47 +0200

    Update main.py

--- wsd/main.py
+++ wsd/main.py
@@ -1,10 +1,10 @@
 import asyncio
-from webserver import start_http_server
+#from webserver import start_http_server
 #import webserver
 #from wsd import UDP_listener_3702, heartbeat_monitor, handle_scan_job
 from wsd import UDP_listener_3702, heartbeat_monitor
 #import wsd
-from config import WSD_HTTP_PORT, WSD_OFFLINE_TIMEOUT, WSD_SCAN_FOLDER
+from config import HTTP_PORT, OFFLINE_TIMEOUT, SCAN_FOLDER
 #import config
 #from scanner import Scanner, SCANNERS
 from scanner import Scanner
