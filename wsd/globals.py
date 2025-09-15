fb7e12a7c2916ebca2dd19ef093b28c780697c8f
rossi75 <91349581+rossi75@users.noreply.github.com>
2025-09-15 12:17:50 +0200

    Update globals.py

--- wsd/globals.py
+++ wsd/globals.py
@@ -33,12 +33,11 @@ NAMESPACES = {
 # -----------------  Nach jedem Update: Liste loggen  -----------------
 def list_scanners():
     logger.info("[SCANNERS] registered Scanners:")
-#        for s in SCANNERS.values():
-#            logger.info(f"  - {s.name} ({s.ip}, {s.uuid})")
+
     for i, s in enumerate(SCANNERS.values(), start=1):
         logger.info(f"[{i}] {s.friendly_name} IP={s.ip} UUID={s.uuid} Online={s.online}")
-        logger.info(f"      ---> XADDR={s.xaddr}")
-        logger.info(f"      ---> last_seen={s.last_seen}")
+        logger.info(f"      --->     XADDR = {s.xaddr}")
+        logger.info(f"      ---> last_seen = {s.last_seen}")
 
 
 
