9dade07dd67e01883168c2cc4671d7855e630b1a
rossi75 <91349581+rossi75@users.noreply.github.com>
2025-09-16 21:31:30 +0200

    Update main.py

--- wsd/main.py
+++ wsd/main.py
@@ -1,24 +1,14 @@
 import asyncio
 from webserver import start_http_server
-#import webserver
-#from wsd import UDP_listener_3702, heartbeat_monitor, handle_scan_job
-#from wsd import UDP_listener_3702, heartbeat_monitor
-from wsd import UDP_listener_3702, probe_monitor
-#import wsd
-from config import HTTP_PORT, OFFLINE_TIMEOUT, SCAN_FOLDER
-#import config
-#from scanner import Scanner, SCANNERS
-from scanner import Scanner
-#import scanner
-#from state import SCANNERS
-#from globals import SCANNERS
+from wsd import UDP_listener_3702, state_monitor
+#from config import HTTP_PORT, OFFLINE_TIMEOUT, SCAN_FOLDER
+#from scanner import Scanner
 
 async def main():
     await asyncio.gather(
         start_http_server(),
         UDP_listener_3702(),
-#        heartbeat_monitor(),
-        probe_monitor(),
+        state_monitor(),
     )
 
 if __name__ == "__main__":
