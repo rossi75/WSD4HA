6e0e2a7b3f0f2d975301f74f23eb3f29fb1b3d34
rossi75 <91349581+rossi75@users.noreply.github.com>
2025-09-22 14:24:35 +0200

    Update templates.py

--- wsd/templates.py
+++ wsd/templates.py
@@ -20,14 +20,14 @@ TEMPLATE_SOAP_TRANSFER_GET = """<?xml version="1.0" encoding="utf-8"?>
 <soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope"
                xmlns:wsa="http://schemas.xmlsoap.org/ws/2004/08/addressing">
   <soap:Header>
-    <wsa:To>{to_device_uuid}</wsa:To>
+    <wsa:To>urn:uuid:{to_device_uuid}</wsa:To>
     <wsa:Action>http://schemas.xmlsoap.org/ws/2004/09/transfer/Get</wsa:Action>
     <wsa:MessageID>urn:uuid:{msg_id}</wsa:MessageID>
     <wsa:ReplyTo>
       <wsa:Address>http://schemas.xmlsoap.org/ws/2004/08/addressing/role/anonymous</wsa:Address>
     </wsa:ReplyTo>
     <wsa:From>
-      <wsa:Address>{from_uuid}</wsa:Address>
+      <wsa:Address>urn:uuid:{from_uuid}</wsa:Address>
     </wsa:From>
   </soap:Header>
   <soap:Body/>
