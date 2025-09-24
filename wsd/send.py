bb5a574b35f2cc1f5993b69be08cc5456215d5b5
rossi75 <91349581+rossi75@users.noreply.github.com>
2025-09-24 10:29:47 +0200

    Update send.py

--- wsd/send.py
+++ wsd/send.py
@@ -15,7 +15,7 @@ from config import OFFLINE_TIMEOUT, LOCAL_IP, HTTP_PORT, FROM_UUID, DISPLAY
 from globals import SCANNERS, list_scanners, NAMESPACES, STATE
 from pathlib import Path
 from scanner import Scanner
-from templates import TEMPLATE_SOAP_PROBE, TEMPLATE_SOAP_TRANSFER_GET
+from templates import TEMPLATE_SOAP_PROBE, TEMPLATE_SOAP_TRANSFER_GET, TEMPLATE_SUBSCRIBE_SAE
 from parse import parse_wsd_packet, parse_probe, parse_transfer_get, parse_subscribe
 
 logging.basicConfig(level=logging.INFO, format='[%(levelname)s] %(message)s')
