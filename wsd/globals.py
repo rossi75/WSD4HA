e32ff2a5641a2dac09e5c3dd61c6e542412eae9f
rossi75 <91349581+rossi75@users.noreply.github.com>
2025-09-26 22:09:52 +0200

    Update globals.py

--- wsd/globals.py
+++ wsd/globals.py
@@ -92,7 +92,7 @@ class STATE(str, Enum):
 
 # -----------------  Nach jedem Update: Liste loggen  -----------------
 def list_scanners():
-    logger.info("[SCANNERS] registered Scanners:")
+    logger.info("[SCANNERS] known Scanners:")
 
     for i, s in enumerate(SCANNERS.values(), start=1):
         logger.info(f"[{i}] {s.friendly_name} @ {s.ip}   UUID={s.uuid}")
