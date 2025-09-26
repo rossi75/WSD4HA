812c8d6cfb004363252956c74c753bd3dca92e3d
rossi75 <91349581+rossi75@users.noreply.github.com>
2025-09-26 10:37:19 +0200

    Update send.py

--- wsd/send.py
+++ wsd/send.py
@@ -75,7 +75,8 @@ async def send_transfer_get(tf_g_uuid: str):
 
     headers = {
         "Content-Type": "application/soap+xml",
-        "User-Agent": "WSDAPI",
+#        "User-Agent": "WSDAPI",
+        "User-Agent": USER_AGENT,
     }
 
     url = SCANNERS[tf_g_uuid].xaddr  # z.B. http://192.168.0.3:8018/wsd
@@ -105,10 +106,8 @@ async def send_transfer_get(tf_g_uuid: str):
     logger.debug(f"TransferGet von {SCANNERS[tf_g_uuid].ip}:\n{body}")
     parse_transfer_get(body, tf_g_uuid)
 
-
 # ---------------- Subscribe ScanAvailableEvent ----------------
 async def send_subscr_ScanAvailableEvent(sae_uuid: str):
-
     # to_device_uuid = scanners endpoint UUID
     # msg_id = Message ID
     # xaddr = serviceadress  ==>  <wsa:To>http://192.168.0.3:8018/wsd/scan</wsa:To>
@@ -129,13 +128,13 @@ async def send_subscr_ScanAvailableEvent(sae_uuid: str):
         xaddr = SCANNERS[sae_uuid].xaddr,
         EndTo_addr = "http://192.168.0.10:5357/asdjkfhewjkhauiscndiausdnue",
         scan_to_name = DISPLAY,
-#        Ref_ID = "680be7cf-bc5a-409d-ad1d-4d6d96b5cb4f",
         Ref_ID = ref_id,
     )
 
     headers = {
         "Content-Type": "application/soap+xml",
-        "User-Agent": "WSDAPI",
+#        "User-Agent": "WSDAPI",
+        "User-Agent": USER_AGENT,
     }
 
     url = SCANNERS[sae_uuid].xaddr  # z.B. http://192.168.0.3:8018/wsd
@@ -143,9 +142,9 @@ async def send_subscr_ScanAvailableEvent(sae_uuid: str):
     logger.debug(f"   --->      TO: {sae_uuid}")
     logger.debug(f"   --->  MSG_ID: {msg_id}")
     logger.debug(f"   --->    FROM: {FROM_UUID}")
-    #logger.debug(f"   --->  End_To: {msg_id}")
-    logger.debug(f"   --->    NAME: {DISPLAY}")
-    #logger.debug(f"   --->  REF_ID: {msg_id}")
+    logger.info(f"   --->  End_To: {EndTo_addr}")
+    logger.info(f"   --->    NAME: {DISPLAY}")
+    logger.info(f"   --->  REF_ID: {ref_id}")
     logger.info(f"   --->     URL: {url}")
     logger.info(f"   --->     XML:\n{xml}")
 
@@ -163,68 +162,6 @@ async def send_subscr_ScanAvailableEvent(sae_uuid: str):
         except Exception as e:
             logger.error(f"[SEND:sae] failed for {SCANNERS[tf_g_uuid].uuid}: {e}")
             SCANNERS[tf_g_uuid].state = STATE.ERROR
-            return None
- 
-#    logger.debug(f"received ScanAvailableEvents from {SCANNERS[sae_uuid].ip} as XML:\n{body}")
-    parse_subscribe(sae_uuid, body)
-
-# ---------------- Subscribe ScanAvailableEvent ----------------
-async def send_subscr_ScanAvailableEvent(sae_uuid: str):
-
-    # to_device_uuid = scanners endpoint UUID
-    # msg_id = Message ID
-    # xaddr = serviceadress  ==>  <wsa:To>http://192.168.0.3:8018/wsd/scan</wsa:To>
-    # from_uuid = WSD4HAs UUID
-    # EndTo_addr = adress that needs to be reachable by the scanner  ==>  <wsa:Address>http://192.168.0.1:5357/6ccf7716-4dc8-47bf-aca4-5a2ae5a959ca</wsa:Address>
-    # scan_to_name = Option selected by the user to start the scanning  ==>  "Scan to Home Assistant"
-    # Ref_ID = one more senseless ID  ==>  <wse:Identifier>urn:uuid:680be7cf-bc5a-409d-ad1d-4d6d96b5cb4f</wse:Identifier>
-    logger.info(f"{datetime.datetime.now():%Y-%m-%d %H:%M:%S} [SEND:subscr_sae] subscribing ScanAvailableEvent to {sae_uuid} @ {SCANNERS[sae_uuid].ip}")
-    
-    SCANNERS[sae_uuid].state = STATE.SUBSCRIBING_SCAN_AVAIL_EVT
-    msg_id = uuid.uuid4()
-    ref_id = uuid.uuid4()
-
-    xml = TEMPLATE_SUBSCRIBE_SAE.format(
-        to_device_uuid = sae_uuid,
-        msg_id = msg_id,
-        from_uuid = FROM_UUID,
-        xaddr = SCANNERS[sae_uuid].xaddr,
-        EndTo_addr = "http://192.168.0.10:5357/asdjkfhewjkhauiscndiausdnue",
-        scan_to_name = DISPLAY,
-#        Ref_ID = "680be7cf-bc5a-409d-ad1d-4d6d96b5cb4f",
-        Ref_ID = ref_id,
-    )
-
-    headers = {
-        "Content-Type": "application/soap+xml",
-        "User-Agent": "WSDAPI",
-    }
-
-    url = SCANNERS[sae_uuid].xaddr  # z.B. http://192.168.0.3:8018/wsd
-
-    logger.debug(f"   --->      TO: {sae_uuid}")
-    logger.debug(f"   --->  MSG_ID: {msg_id}")
-    logger.debug(f"   --->    FROM: {FROM_UUID}")
-    #logger.debug(f"   --->  End_To: {msg_id}")
-    logger.debug(f"   --->    NAME: {DISPLAY}")
-    #logger.debug(f"   --->  REF_ID: {msg_id}")
-    logger.info(f"   --->     URL: {url}")
-    logger.info(f"   --->     XML:\n{xml}")
-
-    body = ""
-
-    async with aiohttp.ClientSession() as session:
-        try:
-            async with session.post(url, data=xml, headers=headers, timeout=5) as resp:
-                if resp.status == 200:
-                    body = await resp.text()
-                else:
-                    SCANNERS[sae_uuid].state = STATE.ERROR
-                    logger.error(f"[SEND:sae] Subscribe to ScanAvailableEvents failed with Statuscode {resp.status}")
-                    return None
-        except Exception as e:
-            logger.error(f"[SEND:sae] failed for {SCANNERS[sae_uuid].uuid}: {e}")
-            SCANNERS[sae_uuid].state = STATE.ERROR
             return None
  
 #    logger.debug(f"received ScanAvailableEvents from {SCANNERS[sae_uuid].ip} as XML:\n{body}")
