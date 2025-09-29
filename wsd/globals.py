9d386716e084baea5785e5c43b808d4b8d4ff3c3
rossi75 <91349581+rossi75@users.noreply.github.com>
2025-09-29 17:40:01 +0200

    Update globals.py

--- wsd/globals.py
+++ wsd/globals.py
@@ -98,7 +98,7 @@ def list_scanners():
     logger.info("[SCANNERS] known Scanners:")
 
     for i, s in enumerate(SCANNERS.values(), start=1):
-        logger.info(f"  [{i}] {s.friendly_name} @ {s.ip}")
+        logger.info(f"  [{i}] {s.friendly_name or s.uuid} @ {s.ip}")
         logger.debug(f"      --->     XADDR: {s.xaddr}")
         logger.info(f"      --->    Status: {s.state.value}")
         logger.debug(f"      ---> last_seen: {s.last_seen}")
