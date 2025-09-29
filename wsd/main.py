283419751bfc5b3853bc427f776908a4534b4fb6
rossi75 <91349581+rossi75@users.noreply.github.com>
2025-09-29 12:09:56 +0200

    Update main.py

--- wsd/main.py
+++ wsd/main.py
@@ -1,5 +1,5 @@
 import asyncio
-from webserver import start_http_server, start_notify_server
+from server import start_http_server, start_notify_server
 from wsd import UDP_listener_3702, state_monitor
 #from config import HTTP_PORT, OFFLINE_TIMEOUT, SCAN_FOLDER
 #from scanner import Scanner
