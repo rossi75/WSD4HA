6c5abcbbfdd3b1198fc65e25a4e6a281b6da3dc5
rossi75 <91349581+rossi75@users.noreply.github.com>
2025-09-29 17:51:25 +0200

    Update globals.py

--- wsd/globals.py
+++ wsd/globals.py
@@ -96,9 +96,11 @@ class STATE(str, Enum):
 # -----------------  Nach jedem Update: Liste loggen  -----------------
 def list_scanners():
     if SCANNERS:
-      logger.info("[SCANNERS] known Scanners:")
-      for i, s in enumerate(SCANNERS.values(), start=1):
-          logger.info(f"  [{i}] {s.friendly_name or s.uuid} @ {s.ip}")
-          logger.debug(f"      --->     XADDR: {s.xaddr}")
-          logger.info(f"      --->    Status: {s.state.value}")
-          logger.debug(f"      ---> last_seen: {s.last_seen}")
+        logger.info("   ------>   known Scanners   <------")
+        for i, s in enumerate(SCANNERS.values(), start=1):
+            logger.info(f"  [{i}] {s.friendly_name or s.uuid} @ {s.ip}")
+            logger.debug(f"      --->     XADDR: {s.xaddr}")
+            logger.info(f"      --->    Status: {s.state.value}")
+            logger.debug(f"      ---> last_seen: {s.last_seen}")
+    else:
+        logger.info("no known Scanners in list")  
