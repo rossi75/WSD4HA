ef42749ca39fb4dda32ff8d9aa798450b7163854
rossi75 <91349581+rossi75@users.noreply.github.com>
2025-10-13 11:07:18 +0200

    Update send.py

--- wsd/send.py
+++ wsd/send.py
@@ -14,7 +14,8 @@ import xml.etree.ElementTree as ET
 #from config import OFFLINE_TIMEOUT, LOCAL_IP, HTTP_PORT, FROM_UUID, DISPLAY, NOTIFY_PORT, get_local_ip
 #from globals import SCANNERS, list_scanners, NAMESPACES, STATE, USER_AGENT, LOG_LEVEL
 from config import OFFLINE_TIMEOUT, LOCAL_IP, HTTP_PORT, FROM_UUID, DISPLAY, NOTIFY_PORT
-from globals import SCANNERS, NAMESPACES, STATE, USER_AGENT, LOG_LEVEL
+#from globals import SCANNERS, NAMESPACES, STATE, USER_AGENT, LOG_LEVEL
+from globals import SCANNERS, NAMESPACES, STATE, USER_AGENT, logger
 from parse import parse_wsd_packet, parse_probe, parse_transfer_get, parse_subscribe, parse_create_scan_job_response, parse_retrieve_image_response
 from pathlib import Path
 from scanner import Scanner
@@ -23,8 +24,8 @@ from templates import TEMPLATE_SOAP_PROBE, TEMPLATE_SOAP_TRANSFER_GET, TEMPLATE_
 
 #logging.basicConfig(level=logging.INFO, format='[%(levelname)s] %(message)s')
 #logging.basicConfig(level=logging.{LOG_LEVEL}, format='[%(levelname)s] %(message)s')
-logging.basicConfig(level=LOG_LEVEL, format='[%(levelname)s] %(message)s')
-logger = logging.getLogger("wsd-addon")
+#logging.basicConfig(level=LOG_LEVEL, format='[%(levelname)s] %(message)s')
+#logger = logging.getLogger("wsd-addon")
 
 # ---------------- Send Scanner Probe ----------------
 async def send_probe(probe_uuid: str):
