c7f2f1f4cf9668cb10852f1e4457e60e18659808
rossi75 <91349581+rossi75@users.noreply.github.com>
2025-10-15 15:59:54 +0200

    Update globals.py

--- wsd/globals.py
+++ wsd/globals.py
@@ -103,7 +103,7 @@ class STATE(str, Enum):
     SCAN_PENDING = "Document available"
     SCAN_REQ_TICKET = "Requesting Job Ticket"
     SCAN_RETRIEVE_IMG = "receiving/downloading a Document"
-    SCAN_SAVING = "saving Document"
+    SCAN_SAVING_DOCUMENT = "saving Document"
     SCAN_DONE = "Document scan done successfully"
     SCAN_FAILED = "Document scan failed"
     ABSENT = "absent"
