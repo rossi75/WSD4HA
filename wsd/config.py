b4dd430f8976ec267bf76f3339de1638b7209981
rossi75 <91349581+rossi75@users.noreply.github.com>
2025-10-15 21:57:38 +0200

    Update config.py

--- wsd/config.py
+++ wsd/config.py
@@ -10,9 +10,7 @@ import re
 import xml.etree.ElementTree as ET
 import subprocess
 import uuid
-#from globals import SCANNERS, list_scanners, FROM_UUID, USER_AGENT, LOG_LEVEL
 from globals import SCANNERS, FROM_UUID, USER_AGENT, LOG_LEVEL, SCAN_FOLDER, STARTUP_DT, logger
-#from globals import SCANNERS, SCAN_JOBS, NAMESPACES, STATE, logger
 from tools import list_scanners, check_port, get_local_ip
 
 # ----------------- To Do -----------------
