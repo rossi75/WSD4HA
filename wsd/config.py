b2b67f6cbb98e8e97bd96fa29842a27a2f15cbd5
rossi75 <91349581+rossi75@users.noreply.github.com>
2026-05-29 20:45:36 +0200

    Update config.py

--- wsd/config.py
+++ wsd/config.py
@@ -106,7 +106,8 @@ logger.info(f" TMUX env: {TMUX}")
 
 # ---------------- SCAN-Folder Path ----------------
 #globals.SCAN_FOLDER = Path(os.environ.get("SCAN_FOLDER", "/share/scans"))
-globals.SCAN_FOLDER = Path(os.environ.get("SCAN_FOLDER", "/config/wsd4ha/scans"))
+#globals.SCAN_FOLDER = Path(os.environ.get("SCAN_FOLDER", "/config/wsd4ha/scans"))
+globals.SCAN_FOLDER = Path(os.environ.get("SCAN_FOLDER", "/share/wsd4ha/scans"))
 #SCAN_FOLDER.mkdir(parents=True, exist_ok=True)
 logger.info(f"global Scan-Path: {globals.SCAN_FOLDER}")
 
