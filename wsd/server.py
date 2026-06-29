cf8214bcdadb933f47bf6d68f7923ca39be40df4
rossi75 <91349581+rossi75@users.noreply.github.com>
2026-06-29 23:44:01 +0200

    Update server.py

--- wsd/server.py
+++ wsd/server.py
@@ -10,7 +10,8 @@ import re
 import xml.etree.ElementTree as ET
 import subprocess
 from config import OFFLINE_TIMEOUT, SCAN_FOLDER, HTTP_PORT, MAX_FILES, NOTIFY_PORT
-from globals import LISTENING_UDP_3702_WSD, LISTENING_TCP_5357_NOTIFY, SCANNERS, SCAN_JOBS, NAMESPACES, STATE, USER_AGENT, logger
+#from globals import LISTENING_UDP_3702_WSD, LISTENING_TCP_5357_NOTIFY, SCANNERS, SCAN_JOBS, logger
+import globals
 from parse import parse_notify_msg
 from tools import find_scanner_by_endto_addr
 from scan_job import run_scan_job
@@ -21,7 +22,7 @@ from scan_job import run_scan_job
 async def download_file(request):
     filename = os.path.basename(request.match_info["filename"])
     filepath = os.path.join(SCAN_FOLDER, filename)
-    logger.info(f"[SERVER:download] received download request for {filename}, which is at {filepath}")
+    globals.logger.info(f"[SERVER:download] received download request for {filename}, which is at {filepath}")
 
     if not os.path.isfile(filepath):
         raise web.HTTPNotFound(text="File not found")
@@ -34,7 +35,7 @@ async def download_file(request):
 async def delete_file(request):
     filename = os.path.basename(request.match_info["filename"])
     filepath = os.path.join(SCAN_FOLDER, filename)
-    logger.info(f"[SERVER:del] received request for delete {filename}, which is at {filepath}")
+    globals.logger.info(f"[SERVER:del] received request for delete {filename}, which is at {filepath}")
 
     if not os.path.isfile(filepath):
         raise web.HTTPNotFound(text="File not found")
@@ -49,16 +50,16 @@ async def delete_file(request):
 # http://homeassistant:8110/pin/{uuid}
 async def pin_scanner_handler(request):
     uuid = request.match_info["uuid"]
-    logger.info(f"[SERVER:pin] received pinning request for {uuid}")
-    SCANNERS[uuid].pin_scanner()
+    globals.logger.info(f"[SERVER:pin] received pinning request for {uuid}")
+    globals.SCANNERS[uuid].pin_scanner()
     raise web.HTTPFound("/")
 
 # ---------------- unpin a Scanner from File ----------------
 # http://homeassistant:8110/unpin/{uuid}
 async def unpin_scanner_handler(request):
     uuid = request.match_info["uuid"]
-    logger.info(f"[SERVER:unpin] received unpinning request for {uuid}")
-    SCANNERS[uuid].unpin_scanner()
+    globals.logger.info(f"[SERVER:unpin] received unpinning request for {uuid}")
+    globals.SCANNERS[uuid].unpin_scanner()
     await asyncio.sleep(2)
     raise web.HTTPFound("/")
 
@@ -67,29 +68,27 @@ async def unpin_scanner_handler(request):
 async def rename_file(request):
     old_name = os.path.basename(request.match_info["oldname"])
     new_name = os.path.basename(request.match_info["newname"])
-    logger.info(f"[SERVER:rename] received renaming request for {old_name} to {new_name}")
+    globals.logger.info(f"[SERVER:rename] received renaming request for {old_name} to {new_name}")
 
     _, old_ext = os.path.splitext(old_name)
     _, new_ext = os.path.splitext(new_name)
-#    old_ext = os.path.splitext(old_name)
-#    new_ext = os.path.splitext(new_name)
-    logger.info(f"old-ext={old_ext}, new-ext={new_ext}")
+    globals.logger.info(f"old-ext={old_ext}, new-ext={new_ext}")
     if new_ext == "":                   # Falls keine neue Endung angegeben wurde,
         new_name += old_ext              # die alte übernehmen.
-        logger.info(f"missing file extension for new filename, added old extension: new-name={new_name}")
+        globals.logger.info(f"missing file extension for new filename, added old extension: new-name={new_name}")
 
     if "/" in new_name or "\\" in new_name:
-        logger.info("declined renaming request due to invalid characters in filename")
+        globals.logger.info("declined renaming request due to invalid characters in filename")
         raise web.HTTPBadRequest(text="Invalid filename")
 
     old_path = os.path.join(SCAN_FOLDER, old_name)
     new_path = os.path.join(SCAN_FOLDER, new_name)
-    logger.info(f"old-path={old_path}, new-path={new_path}")
+    globals.logger.info(f"old-path={old_path}, new-path={new_path}")
     if not os.path.isfile(old_path):
-        logger.info(f"could not find old-path {old_path}")
+        globals.logger.info(f"could not find old-path {old_path}")
         raise web.HTTPNotFound(text=f"could not find old file {old_path}, cannot proceed")
     if os.path.exists(new_path):
-        logger.info(f"new-path {new_path} still exists, cannot change name")
+        globals.logger.info(f"new-path {new_path} still exists, cannot change name")
         raise web.HTTPConflict(text=f"new file {new_path} already exists")
 
     try:
@@ -100,36 +99,36 @@ async def rename_file(request):
 
 # ---------------- HTTP Server ----------------
 async def start_http_server():
-    logger.info(f"[SERVER:start_http] configuring HTTP Server for UI on Port {HTTP_PORT}")
+    globals.logger.info(f"[SERVER:start_http] configuring HTTP Server for UI on Port {HTTP_PORT}")
     app = web.Application()
     app.router.add_get("/", status_page)
-    logger.info("   ---> added endpoint /")
+    globals.logger.info("   ---> added endpoint /")
     app.router.add_get("/download/{filename}", download_file)
-    logger.info("   ---> added endpoint /download/{filename}")
+    globals.logger.info("   ---> added endpoint /download/{filename}")
     app.router.add_get("/delete/{filename}", delete_file)
-    logger.info("   ---> added endpoint /delete/{filename}")
+    globals.logger.info("   ---> added endpoint /delete/{filename}")
     app.router.add_get("/pin/{uuid}", pin_scanner_handler)
-    logger.info("   ---> added endpoint /pin/{uuid}")
+    globals.logger.info("   ---> added endpoint /pin/{uuid}")
     app.router.add_get("/unpin/{uuid}", unpin_scanner_handler)
-    logger.info("   ---> added endpoint /unpin/{uuid}")
+    globals.logger.info("   ---> added endpoint /unpin/{uuid}")
     app.router.add_get("/rename/{oldname}/{newname}", rename_file)
-    logger.info("   ---> added endpoint /rename/{oldname}/{newname}")
+    globals.logger.info("   ---> added endpoint /rename/{oldname}/{newname}")
     runner = web.AppRunner(app)
     await runner.setup()
-    logger.debug(f"   ---> runner.setup().web.AppRunner(app)")
+    globals.logger.debug(f"   ---> runner.setup().web.AppRunner(app)")
     
     # An alle Interfaces binden (0.0.0.0) -> wichtig für Docker / HA
     site = web.TCPSite(runner, "0.0.0.0", HTTP_PORT)
     await site.start()
-    logger.info(f"HTTP Server for UI is running on Port {HTTP_PORT}")
+    globals.logger.info(f"HTTP Server for UI is running on Port {HTTP_PORT}")
 
 # ---------------- WebUI ----------------
 async def status_page(request):
-    logger.info(f"{datetime.datetime.now():%Y-%m-%d %H:%M:%S} [SERVER:status_page] received request for status page")
+    globals.logger.info(f"{datetime.datetime.now():%Y-%m-%d %H:%M:%S} [SERVER:status_page] received request for status page")
 
     # Scanner
     scanner_list = ''
-    for s in SCANNERS.values():
+    for s in globals.SCANNERS.values():
         if s.pinned:
             pin_button = (
                 f"Pinned<br>"
@@ -170,7 +169,7 @@ async def status_page(request):
 
     # Jobs
     job_list = ''
-    for j in SCAN_JOBS.values():
+    for j in globals.SCAN_JOBS.values():
         job_list = "<tr style='color:{color}'>"
         job_list += f"<td style='text-align:center;'>{j.scanjob_identifier}</td>"
         job_list += f"<td style='text-align:center;'>{j.input_source}</td>"
@@ -188,13 +187,13 @@ async def status_page(request):
     # Dateien
     files = sorted(SCAN_FOLDER.iterdir(), key=lambda f: f.stat().st_mtime, reverse=True)[:MAX_FILES]
     file_list = ''
-    logger.info(f"files from {SCAN_FOLDER}:")
+    globals.logger.info(f"files from {SCAN_FOLDER}:")
     for f in files:
         stat = f.stat()
         timestamp = datetime.datetime.fromtimestamp(stat.st_mtime).strftime("%Y-%m-%d %H:%M:%S")
         size_kb = stat.st_size / 1024
         filepath = f"{SCAN_FOLDER}/{f.name}"
-        logger.info(f"{timestamp}, {size_kb:.1f} kB, {filepath}")
+        globals.logger.info(f"{timestamp}, {size_kb:.1f} kB, {filepath}")
         file_list += (
             f"<tr>"
             f"<td><a href='/download/{f.name}'>{f.name}</a></td>"
@@ -212,18 +211,18 @@ async def status_page(request):
 
     statuscolor_udp_3704_wsd = (
         "limegreen"
-        if LISTENING_UDP_3702_WSD
+        if globals.LISTENING_UDP_3702_WSD
         else "lightgray"
     )
     statuscolor_tcp_5357_notify = (
         "limegreen"
-        if LISTENING_TCP_5357_NOTIFY
+        if globals.LISTENING_TCP_5357_NOTIFY
         else "lightgray"
     )
-    logger.info(f"udp_3702_wsd={LISTENING_UDP_3702_WSD}, tcp_5357_notify={LISTENING_TCP_5357_NOTIFY}")
+    globals.logger.info(f"udp_3702_wsd={globals.LISTENING_UDP_3702_WSD}, tcp_5357_notify={globals.LISTENING_TCP_5357_NOTIFY}")
 
     
-    logger.debug(f"   ---> probably delivering http-response")
+    globals.logger.debug(f"   ---> probably delivering http-response")
     return web.Response(text=f"""
         <html>
         <head>
@@ -281,52 +280,49 @@ async def status_page(request):
 # parallel zum UI starten
 # ---------------- NOTIFY Server ----------------
 async def start_notify_server():
-    logger.info(f"[SERVER:start_notify] configuring Notify Server on Port {NOTIFY_PORT}")
+    globals.logger.info(f"[SERVER:start_notify] configuring Notify Server on Port {NOTIFY_PORT}")
     app = web.Application()
     app.add_routes(routes)
 
     runner = web.AppRunner(app)
     try:
         await runner.setup()
-        logger.debug(f"   ---> runner.setup().web.AppRunner(app)")
+        globals.logger.debug(f"   ---> runner.setup().web.AppRunner(app)")
     
         # An alle Interfaces binden (0.0.0.0) -> wichtig für Docker / HA
         site = web.TCPSite(runner, "0.0.0.0", NOTIFY_PORT)
         await site.start()
-        #globals.LISTENING_TCP_5357_NOTIFY = True
-        LISTENING_TCP_5357_NOTIFY = True
-        logger.info(f"Notify Server is running on Port {NOTIFY_PORT}")
+        globals.LISTENING_TCP_5357_NOTIFY = True
+        globals.logger.info(f"Notify Server is running on Port {NOTIFY_PORT}")
     except OSError as e:
-        #globals.LISTENING_TCP_5357_NOTIFY = False
-        LISTENING_TCP_5357_NOTIFY = False
-        logger.error(f"Could not start Notify Server on TCP/{NOTIFY_PORT}: {e}")
+        globals.LISTENING_TCP_5357_NOTIFY = False
+        globals.logger.error(f"Could not start Notify Server on TCP/{NOTIFY_PORT}: {e}")
         return
     except Exception as e:
-        #globals.LISTENING_TCP_5357_NOTIFY = False
-        LISTENING_TCP_5357_NOTIFY = False
-        logger.exception(f"Unexpected error while starting Notify Server: {e}")
+        globals.LISTENING_TCP_5357_NOTIFY = False
+        globals.logger.exception(f"Unexpected error while starting Notify Server: {e}")
         return        
 
-    logger.info(f"***************************************************************************************************************")
-    logger.info(f"*                                                E V E N T S                                                  *")
-    logger.info(f"***************************************************************************************************************")
+    globals.logger.info(f"***************************************************************************************************************")
+    globals.logger.info(f"*                                                E V E N T S                                                  *")
+    globals.logger.info(f"***************************************************************************************************************")
 # ---------------- NOTIFY handler ----------------
 # Fängt alles hinter / ab, z.B. /6ccf7716-4dc8-47bf-aca4-5a2ae5a959ca
 routes = web.RouteTableDef()
 @routes.post(r'/{uuid:[0-9a-fA-F\-]+}')
 async def notify_handler(request):
-    logger.info(f"{datetime.datetime.now():%Y-%m-%d %H:%M:%S} [SERVER:notify_handler] received {request.method} event on {request.path}")
+    globals.logger.info(f"{datetime.datetime.now():%Y-%m-%d %H:%M:%S} [SERVER:notify_handler] received {request.method} event on {request.path}")
     xml_payload = await request.text()
-    logger.debug(f"   ---> XML payload: \n {xml_payload}")
+    globals.logger.debug(f"   ---> XML payload: \n {xml_payload}")
 
     scanner_uuid = ""
     EndTo_id = request.path
     scanner_uuid = find_scanner_by_endto_addr(EndTo_id)
 
     if scanner_uuid is not None:
-        logger.info(f"found scanner {SCANNERS[scanner_uuid].friendly_name or scanner_uuid} @ {SCANNERS[scanner_uuid].ip} for {EndTo_id}")
+        globals.logger.info(f"found scanner {globals.SCANNERS[scanner_uuid].friendly_name or scanner_uuid} @ {globals.SCANNERS[scanner_uuid].ip} for {EndTo_id}")
     else:
-        logger.info(f"no search result for {EndTo_id}")
+        globals.logger.info(f"no search result for {EndTo_id}")
         return web.Response(status=400, text="bad notify endpoint")
 
     scanjob_identifier = None
@@ -334,17 +330,17 @@ async def notify_handler(request):
         root = ET.fromstring(xml_payload)
         scanjob_identifier = parse_notify_msg(scanner_uuid, xml_payload)      # hier werden die Metadaten zum Abholauftrag zusammengetragen
     except Exception as e:
-        logger.warning(f"[SERVER:notify_handler] invalid xml: {e}")
+        globals.logger.warning(f"[SERVER:notify_handler] invalid xml: {e}")
         return web.Response(status=400, text="bad xml")
 
     if not scanjob_identifier:
         return web.Response(status=400, text="could not extract any scanjob identifier from xml")
     
-    logger.info(f"scheduling scan job {scanjob_identifier} for scanner {SCANNERS[scanner_uuid].friendly_name or scanner_uuid}")
+    globals.logger.info(f"scheduling scan job {scanjob_identifier} for scanner {globals.SCANNERS[scanner_uuid].friendly_name or scanner_uuid}")
 
     loop = asyncio.get_event_loop()
     loop.call_soon(asyncio.create_task, run_scan_job(scanjob_identifier))
-    logger.debug(f"task should start soon...!")
+    globals.logger.debug(f"task should start soon...!")
 
     return web.Response(status=202, text="Alles juut")
 
